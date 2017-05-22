186a010c25ed818b8c4b3ee4981c37c9202aa4be
ankatta <anilreddykatta@hotmail.com>
2017-05-22 01:04:58 -0500

    Fixed and validated in IOS

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class RNAccountKit {
     titleType: 'login',
     initialAuthState: '',
     initialEmail: '',
+    initialCountryCode: '',
     initialPhone: '',
     facebookNotificationsEnabled: true,
     readPhoneStateEnabled: true,
