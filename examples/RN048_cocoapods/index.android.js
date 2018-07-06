adec29c984a331c9a72e41933a0f5671f87e7d90
Axel Schenkelman <axel.schenkelman@gmail.com>
2018-07-06 16:06:09 -0300

    Add RN048 example with cocoapods

---
+++ examples/RN048_cocoapods/index.android.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native'
+import AccountKitSample from './app'
+import { getMaxListeners } from 'cluster'
+
+AppRegistry.registerComponent('RN048_cocoapods', () => AccountKitSample)
