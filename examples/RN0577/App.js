7faa08668ec3db1caccc6cecdfb5421425049d5f
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2019-01-13 21:01:20 -0300

    Integrate lib with RN0577 example

--- examples/RN0577/App.js
+++ examples/RN0577/App.js
@@ -1,29 +1,158 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Switch } from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import AccountKit, {
+  LoginButton,
+  Color,
+  StatusBarStyle
+} from "react-native-facebook-account-kit";
 
-type Props = {};
-export default class App extends Component<Props> {
+class AccountKitSample extends Component {
+  state = {
+    authToken: null,
+    loggedAccount: null
+  };
+
+  componentWillMount() {
+    this.configureAccountKit();
+
+    AccountKit.getCurrentAccessToken()
+      .then(token => {
+        if (token) {
+          AccountKit.getCurrentAccount().then(account => {
+            this.setState({
+              authToken: token,
+              loggedAccount: account
+            });
+          });
+        } else {
+          console.log("No user account logged");
+        }
+      })
+      .catch(e => console.log("Failed to get current access token", e));
+  }
+
+  configureAccountKit() {
+    AccountKit.configure({
+      theme: {
+        //backgroundColor:       Color.rgba(0,120,0,0.1),
+        //buttonBackgroundColor: Color.rgba(0, 153, 0, 1.00),
+        //buttonDisabledBackgroundColor: Color.rgba(100, 153, 0, 0.5),
+        //buttonBorderColor:     Color.rgba(0,255,0,1),
+        //buttonTextColor:       Color.rgba(0,255,0,1),
+        //headerBackgroundColor: Color.rgba(0, 153, 0, 1.00),
+        //headerTextColor:       Color.rgba(0,255,0,1),
+        //headerButtonTextColor: Color.rgba(0,255,0,1),
+        //iconColor:             Color.rgba(0,255,0,1),
+        //inputBackgroundColor:  Color.rgba(0,255,0,1),
+        //inputBorderColor:      Color.hex('#ccc'),
+        //inputTextColor:        Color.hex('#0f0'),
+        //textColor:             Color.hex('#0f0'),
+        //titleColor:            Color.hex('#0f0'),
+        //backgroundImage:       "background.png",
+        //statusBarStyle:        StatusBarStyle.LightContent,
+      },
+      //countryWhitelist: [ "AR", "BR", "US" ],
+      //countryBlacklist: [ "BR" ],
+      //defaultCountry: "AR"
+      initialEmail: "example.com",
+      initialPhoneCountryPrefix: "+355"
+      // initialPhoneNumber: "123-456-7890"
+    });
+  }
+
+  onLogin(token) {
+    if (!token) {
+      console.warn("User canceled login");
+      this.setState({});
+    } else {
+      AccountKit.getCurrentAccount().then(account => {
+        this.setState({
+          authToken: token,
+          loggedAccount: account
+        });
+      });
+    }
+  }
+
+  onLoginError(e) {
+    console.log("Failed to login", e);
+  }
+
+  onEmailLoginPressed() {
+    AccountKit.loginWithEmail()
+      .then(token => {
+        this.onLogin(token);
+      })
+      .catch(e => this.onLoginError(e));
+  }
+
+  onLogoutPressed() {
+    AccountKit.logout()
+      .then(() => {
+        this.setState({
+          authToken: null,
+          loggedAccount: null
+        });
+      })
+      .catch(e => console.log("Failed to logout"));
+  }
+
+  renderUserLogged() {
+    const { id, email, phoneNumber } = this.state.loggedAccount;
+
+    return (
+      <View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.onLogoutPressed()}
+        >
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.label}>Account Kit Id</Text>
+        <Text style={styles.text}>{id}</Text>
+        <Text style={styles.label}>Email</Text>
+        <Text style={styles.text}>{email}</Text>
+        <Text style={styles.label}>Phone Number</Text>
+        <Text style={styles.text}>
+          {phoneNumber
+            ? `${phoneNumber.countryCode} ${phoneNumber.number}`
+            : ""}
+        </Text>
+      </View>
+    );
+  }
+
+  renderLogin() {
+    return (
+      <View>
+        <LoginButton
+          style={styles.button}
+          type="phone"
+          onLogin={token => this.onLogin(token)}
+          onError={e => this.onLogin(e)}
+        >
+          <Text style={styles.buttonText}>SMS</Text>
+        </LoginButton>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.onEmailLoginPressed()}
+        >
+          <Text style={styles.buttonText}>Email</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {this.state.loggedAccount
+          ? this.renderUserLogged()
+          : this.renderLogin()}
       </View>
     );
   }
@@ -32,18 +161,32 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  welcome: {
+  button: {
+    height: 50,
+    width: 300,
+    backgroundColor: "aqua",
+    marginBottom: 10
+  },
+  buttonText: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  label: {
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginTop: 20
   },
+  text: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  }
 });
+
+export default AccountKitSample;
--- examples/RN0577/android/app/src/main/java/com/rn0577/MainApplication.java
+++ examples/RN0577/android/app/src/main/java/com/rn0577/MainApplication.java
@@ -3,6 +3,7 @@ package com.rn0577;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.underscope.react.fbak.RNAccountKitPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNAccountKitPackage()
       );
     }
 
